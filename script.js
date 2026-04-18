750a5619f6a53ae7048a3f3cb07eab3b44ef7fd0
Kishore <stonebench04@gmail.com>
2026-04-18 11:30:02 +0530

    added helpdesk project

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ console.log("Portfolio loaded successfully!");
 function scrollToProjects() {
     document.getElementById("projects").scrollIntoView();
 }
+const cards = document.querySelectorAll(".project-card");
+
+window.addEventListener("scroll", () => {
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+
+        if (cardTop < window.innerHeight - 50) {
+            card.style.opacity = 1;
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
